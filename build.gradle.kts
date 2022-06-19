a536bc479c164a470538993fea84356b4826c23b
Lital Amos <42635456+lital-nanit@users.noreply.github.com>
2022-06-19 20:59:04 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,61 +50,10 @@ extensions.findByName("buildScan")?.withGroovyBuilder {
 }
 
 gradlePlugin {
-    plugins {
-        create("pluginMaven") {
-            id = "com.chromaticnoise.multiplatform-swiftpackage"
-            implementationClass = "com.chromaticnoise.multiplatformswiftpackage.MultiplatformSwiftPackagePlugin"
-        }
+    val plug by plugins.creating {
+        id = "com.nanit.multiplatform-swiftpackage"
+        implementationClass = "com.chromaticnoise.multiplatformswiftpackage.MultiplatformSwiftPackagePlugin"
     }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("pluginMaven") {
-            pom {
-                groupId = "com.chromaticnoise.multiplatform-swiftpackage"
-                artifactId = "com.chromaticnoise.multiplatform-swiftpackage.gradle.plugin"
-
-                name.set("Multiplatform Swift Package")
-                description.set("Gradle plugin to generate a Swift.package file and XCFramework to distribute a Kotlin Multiplatform iOS library")
-                url.set("https://github.com/ge-org/multiplatform-swiftpackage")
-
-                licenses {
-                    license {
-                        name.set("Apache License, Version 2.0")
-                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                developers {
-                    developer {
-                        name.set("Georg Dresler")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:https://github.com/ge-org/multiplatform-swiftpackage.git")
-                    developerConnection.set("scm:git:ssh://git@github.com/ge-org/multiplatform-swiftpackage.git")
-                    url.set("https://github.com/ge-org/multiplatform-swiftpackage")
-                }
-            }
-        }
-    }
-
-    repositories {
-        maven {
-            val releasesUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-            val snapshotsUrl = "https://oss.sonatype.org/content/repositories/snapshots/"
-            name = "mavencentral"
-            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsUrl else releasesUrl)
-            credentials {
-                username = System.getenv("SONATYPE_NEXUS_USERNAME")
-                password = System.getenv("SONATYPE_NEXUS_PASSWORD")
-            }
-        }
-    }
-}
-
-signing {
-    sign(publishing.publications["pluginMaven"])
 }
 
 tasks.javadoc {
